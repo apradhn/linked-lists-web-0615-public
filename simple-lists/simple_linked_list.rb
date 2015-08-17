f8033f90ae9bb1fb9dfaba023b1532bf32f72cce
apradhn <abhijit.pradhan6@gmail.com>
2015-08-17 12:47:40 -0400

    finishes simple linked list specs

--- simple-lists/simple_linked_list.rb
+++ simple-lists/simple_linked_list.rb
@@ -1,3 +1,42 @@
+require "pry"
+
 class Element
+  attr_accessor :datum, :next
+
+  def initialize(a, b)
+    self.datum = a
+    self.next = b
+  end
+
+  def self.to_a(element)
+    if element.nil?
+      []
+    else
+      Element.to_a(element.next).unshift(element.datum)
+    end
+  end
+
+  def to_a
+    Element.to_a(self)
+  end
+
+  def self.from_a(arr)
+    arr = arr.to_a if arr.class == Range
+
+    if arr.empty?
+      nil
+    elsif arr.size == 1
+      Element.new(arr[0], nil)
+    else
+      Element.new(arr[0], Element.from_a(arr[1..-1]))
+    end
+  end
+
+
+  def reverse
+    arr = Element.to_a(self)
+    Element.from_a(arr.reverse)
+  end
+
 
 end
