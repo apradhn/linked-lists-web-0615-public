ff25570669f0a559be19f1083ac9213412277d6a
apradhn <abhijit.pradhan6@gmail.com>
2015-08-18 15:05:08 -0400

    finishes lab

--- deque/deque.rb
+++ deque/deque.rb
@@ -1,5 +1,49 @@
 require_relative '../simple-lists/simple_linked_list'
 
 class Deque
+  attr_accessor :list
 
+  def initialize
+    @list = nil
+  end
+
+  def push(datum)
+    if list.nil?
+      self.list = Element.new(datum, nil)
+    elsif list.datum && list.next.nil?
+      list.next = Element.new(datum, nil)
+    elsif list.datum && list.next
+      list.next.next = Element.new(datum, nil)
+    end
+  end
+
+  def pop
+    if list.next.nil?
+      popped_value = list.datum
+    else
+      popped_value = list.next.datum
+    end
+    self.list = Element.new(list.datum, nil)
+    popped_value    
+  end
+
+  def shift
+    shifted_value = list.datum
+    if list.next.nil?
+      self.list = nil
+    elsif list.next.next.nil?
+      self.list = Element.new(self.list.next.datum, nil)
+    else
+      self.list = Element.new(self.list.next.datum, self.list.next.next)
+    end
+    shifted_value
+  end
+
+  def unshift(datum)
+    if list.nil?
+      self.list = Element.new(datum, nil)
+    else 
+      self.list = Element.new(datum, Element.new(list.datum, list.next))
+    end
+  end
 end
--- simple-lists/simple_linked_list.rb
+++ simple-lists/simple_linked_list.rb
@@ -1,11 +1,23 @@
 require "pry"
 
 class Element
-  attr_accessor :datum, :next
+  attr_accessor :datum
+  attr_reader :next, :previous
 
-  def initialize(a, b)
-    self.datum = a
-    self.next = b
+  def previous=(element)
+    @previous = element
+    element.instance_variable_set(:@next, self)
+  end
+
+  def next=(element)
+    @next = element
+    element.instance_variable_set(:@previous, self)
+  end
+
+  def initialize(datum, next_elem)
+    @datum = datum
+    @next = next_elem
+    next_elem.instance_variable_set(:@previous, self) if next_elem.class == Element
   end
 
   def self.to_a(element)
